2030daca4f2f6f36dd00d71a42e6eb6159a97aac
noemiailee <noemip0414@gmail.com>
2026-03-16 21:27:27 -0700

    Update Assignment3.1.cpp

--- Assignment3.1.cpp
+++ Assignment3.1.cpp
@@ -3,8 +3,32 @@
 #include <iomanip>
 using namespace std;
 
-int main()
-{
+int main(){
+    const int NUMSTORES = 5;
+ 
+    const int ASTERISKVALUE = 100;
+    
+  
+    for (int i = 0; i < NUMSTORES; ++i) {
+        cout << "Enter today's sales for store " << i + 1 << ": ";
+        cin >> sales[i];
+    }
 
-return 0;
+    cout << "\nSALES BAR CHART" << endl;
+    cout << "(Each * = $" << ASTERISKVALUE << ")" << endl;
+
+    for (int i = 0; i < NUMSTORES; ++i) {
+        cout << "Store " << i + 1 << ": ";
+        
+     
+        int numAsterisks = sales[i] / ASTERISKVALUE;
+        
+        
+        for (int j = 0; j < numofasterisks; ++j) {
+            cout << "*";
+        }
+        cout << endl; 
+    }
+
+    return 0;
 }
